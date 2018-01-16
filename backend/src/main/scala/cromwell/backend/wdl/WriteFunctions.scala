a5146e5a9bc3a7464cedb3ae70d7983d0be230da
Chris Llanwarne <cjllanwarne@gmail.com>
2018-01-16 18:04:08 -0500

    IWDR: Allow inputs to be relative

--- backend/src/main/scala/cromwell/backend/wdl/WriteFunctions.scala
+++ backend/src/main/scala/cromwell/backend/wdl/WriteFunctions.scala
@@ -1,16 +1,18 @@
 package cromwell.backend.wdl
 
+import java.io.File
+
 import better.files.File.OpenOptions
 import cats.instances.future._
 import cats.syntax.functor._
 import cromwell.core.io.AsyncIoFunctions
-import cromwell.core.path.Path
+import cromwell.core.path.{Path, PathFactory}
 import wom.expression.IoFunctionSet
 import wom.values.WomSingleFile
 
 import scala.concurrent.Future
 
-trait WriteFunctions extends IoFunctionSet with AsyncIoFunctions {
+trait WriteFunctions extends PathFactory with IoFunctionSet with AsyncIoFunctions {
 
   /**
     * Directory that will be used to write files.
@@ -27,8 +29,10 @@ trait WriteFunctions extends IoFunctionSet with AsyncIoFunctions {
     }
   }
 
+  def qualifyPath(pathFrom: String): String = if (new File(pathFrom).isAbsolute) pathFrom else  System.getProperty("user.dir") + "/" + pathFrom
+
   override def copyFile(pathFrom: String, targetName: String): Future[WomSingleFile] = {
-    val source = _writeDirectory.root / pathFrom
+    val source = buildPath(qualifyPath(pathFrom))
     val destination = _writeDirectory / targetName
 
     asyncIo.copyAsync(source, destination).as(WomSingleFile(destination.pathAsString))
