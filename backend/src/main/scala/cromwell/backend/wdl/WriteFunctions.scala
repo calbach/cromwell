f19de10a90cf440c9920dbc48932b993ed53d0e4
Chris Llanwarne <cjllanwarne@gmail.com>
2018-01-17 14:29:23 -0500

    Some fixup?

--- backend/src/main/scala/cromwell/backend/wdl/WriteFunctions.scala
+++ backend/src/main/scala/cromwell/backend/wdl/WriteFunctions.scala
@@ -5,12 +5,15 @@ import java.io.File
 import better.files.File.OpenOptions
 import cats.instances.future._
 import cats.syntax.functor._
+import com.typesafe.config.ConfigFactory
 import cromwell.core.io.AsyncIoFunctions
 import cromwell.core.path.{Path, PathFactory}
 import wom.expression.IoFunctionSet
 import wom.values.WomSingleFile
 
+import net.ceedubs.ficus.Ficus._
 import scala.concurrent.Future
+
 
 trait WriteFunctions extends PathFactory with IoFunctionSet with AsyncIoFunctions {
 
@@ -29,10 +32,19 @@ trait WriteFunctions extends PathFactory with IoFunctionSet with AsyncIoFunction
     }
   }
 
-  def qualifyPath(pathFrom: String): String = if (new File(pathFrom).isAbsolute) pathFrom else  System.getProperty("user.dir") + "/" + pathFrom
+  private val relativeToLocal = System.getProperty("user.dir")
+  private val relativeToPapi = ConfigFactory.load().as[Option[String]]("papi.default-input-gcs-prefix").orElse(ConfigFactory.load().as[Option[String]]("backend.providers.JES.config.root")).getOrElse("gs:/")
+
+  def relativeToAbsolutePath(pathFrom: String, papi: Boolean): String = if (new File(pathFrom).isAbsolute) pathFrom else {
+    val result = (if (papi) relativeToPapi else relativeToLocal) + "/" + pathFrom
+    println(s"RelativeToAbsolute of $pathFrom was $result")
+    result
+  }
 
   override def copyFile(pathFrom: String, targetName: String): Future[WomSingleFile] = {
-    val source = buildPath(qualifyPath(pathFrom))
+    val papi = buildPath(".").toString.startsWith("gs")
+
+    val source = buildPath(relativeToAbsolutePath(pathFrom, papi))
     val destination = _writeDirectory / targetName
 
     asyncIo.copyAsync(source, destination).as(WomSingleFile(destination.pathAsString))
